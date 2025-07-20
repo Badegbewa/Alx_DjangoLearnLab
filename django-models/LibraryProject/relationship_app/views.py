8c20a9523636c99f052fdc0883ce7210a8ddde52
Adegbewa Beyioku <beyiokuenny@gmail.com>
2025-07-20 22:52:59 +0100

    made changes to meet checker

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book 
 
-# Function-based view to list all books
 def list_books(request):
     books = Book.objects.all() 
     return render(request, 'relationship_app/list_books.html', {'books': books}) 
 
-# Class-based view to show library details
+from .models import Library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html' 
