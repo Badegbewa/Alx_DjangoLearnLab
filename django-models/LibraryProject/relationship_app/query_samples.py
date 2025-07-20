67b1ec3cd815f0a5aedabe10470f052210195dbb
Adegbewa Beyioku <beyiokuenny@gmail.com>
2025-07-20 22:04:15 +0100

    made changes to code to meet checker

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,17 +1,20 @@
-from relationship_app.models import Author, Book
+from relationship_app.models import Author, Book, Library, Librarian
 
 # 1. Query all books by a specific author
-author = Author.objects.get(name="Dan Brown")
-books_by_author = Book.objects.filter(author=author)
-print("Books by Dan Brown:", books_by_author)
+author = Author.objects.create(name="Dan Brown")
+book = Book.objects.create(title="Lost Symbol", author=author)
 
-from relationship_app.models import Library, Book
-# 2. List all books in a library
-library = Library.objects.get(name="library_name")
+# 2. List Books in a Library
+library_name = "Central Library"
+library = Library.objects.create(name=library_name)
+library.books.add(book)
+
+library = Library.objects.get(name=library_name)
+
 books_in_library = library.books.all()
-print("Books in Central Library:", books_in_library)
+print(f"Books in {library.name}:", [book.title for book in books_in_library])
 
-from relationship_app.models import Librarian, Library
-# 3. Retrieve the librarian for a library
+# 3. Retrieve the librarian for the library
+librarian = Librarian.objects.create(name="John Eddy", library=library)
 librarian = Librarian.objects.get(library=library)
-print("Librarian for Central Library:", librarian.name)
+print("Librarian:", librarian.name)
