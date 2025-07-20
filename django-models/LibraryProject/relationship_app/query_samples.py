9f6723e19ee5e7769204a11c0fb27f93b74d3a54
Adegbewa Beyioku <beyiokuenny@gmail.com>
2025-07-20 21:23:44 +0100

    created a new app within project file to run relationships

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,17 @@
+from relationship_app.models import Author, Book
+
+# 1. Query all books by a specific author
+author = Author.objects.get(name="Dan Brown")
+books_by_author = Book.objects.filter(author=author)
+print("Books by Dan Brown:", books_by_author)
+
+from relationship_app.models import Library, Book
+# 2. List all books in a library
+library = Library.objects.get(name="Central Library")
+books_in_library = library.books.all()
+print("Books in Central Library:", books_in_library)
+
+from relationship_app.models import Librarian, Library
+# 3. Retrieve the librarian for a library
+librarian = Librarian.objects.get(library=library)
+print("Librarian for Central Library:", librarian.name)
