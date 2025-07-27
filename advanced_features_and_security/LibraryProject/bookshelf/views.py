6c174e1b82646d55a12f65f1d2e3daf162df6474
Adegbewa Beyioku <beyiokuenny@gmail.com>
2025-07-27 23:54:57 +0100

    made changes to meet checker

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,19 +2,19 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
 from .models import Book
 
-# View books (only if user has 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def view_books(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/view_books.html', {'books': books})
 
-# Create a new book (only if user has can_create permission)
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
     return render(request, 'bookshelf/create_book.html')
 
-# Edit a book (only if user has can_edit permission)
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def edit_book(request, book_id):
     return render(request, 'bookshelf/edit_book.html')
 
+def book_list(request):
+    books = Book.objects.all() 
+    return render(request, 'bookshelf/book_list.html', {'books': books})
