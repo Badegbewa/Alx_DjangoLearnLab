f59f596e75974cbbbf5b9fe9ad391f1b33a0aa66
Adegbewa Beyioku <beyiokuenny@gmail.com>
2025-07-27 18:51:49 +0100

    Made changes to admin.py to meet checker

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -7,3 +7,16 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ("publication_year",)
 
 admin.site.register(Book, BookAdmin)
+
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
