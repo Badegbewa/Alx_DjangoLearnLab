5331f7bb9d54e8726d2bf76e483ac6b6bcc5c83e
Adegbewa Beyioku <beyiokuenny@gmail.com>
2025-07-27 01:32:52 +0100

    added to code to manage UserProfile Model adding

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -18,3 +18,12 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin-only/', views.admin_view, name='admin_view'),
+    path('librarian-only/', views.librarian_view, name='librarian_view'),
+    path('member-only/', views.member_view, name='member_view'),
+]
