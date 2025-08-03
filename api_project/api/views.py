654037b4daefd3b1d19f18231cd39fb498caef8d
Adegbewa Beyioku <beyiokuenny@gmail.com>
2025-08-03 18:32:12 +0100

    Created Viewsets from restframework to handle CRUD operations on Book model

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -6,5 +6,15 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for handling all CRUD operations on Book model.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 
