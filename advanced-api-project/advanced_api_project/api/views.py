95d1058018f750e6e66a99a3c0c74db9e5e738a2
Adegbewa Beyioku <beyiokuenny@gmail.com>
2025-08-22 22:41:10 +0100

    creted custom permissions

--- advanced-api-project/advanced_api_project/api/views.py
+++ advanced-api-project/advanced_api_project/api/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookListCreateView(generics.ListCreateAPIView):
+    """
+    GET /books/ → list all books
+    POST /books/ → create a new book (only if logged in)
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_permissions(self):
+        if self.request.method == "POST":
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    GET /books/<id>/ → get one book
+    PUT/PATCH /books/<id>/ → update it (only if logged in)
+    DELETE /books/<id>/ → delete it (only if logged in)
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_permissions(self):
+        if self.request.method in ["PUT", "PATCH", "DELETE"]:
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
+
+def get_queryset(self):
+    queryset = Book.objects.all()
+    year = self.request.query_params.get("year")
+    if year:
+        queryset = queryset.filter(publication_year=year)
+    return queryset
