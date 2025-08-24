9d548ec1eee3e2fc835bb2af75ca78151897494b
Adegbewa Beyioku <beyiokuenny@gmail.com>
2025-08-24 22:18:40 +0100

    made changes to forms.py to meet checker

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post
 from .models import Comment
+from taggit.forms import TagWidget
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -35,5 +36,5 @@ class CommentForm(forms.ModelForm):
         model = Comment
         fields = ['content']
         widgets = {
-            'content': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Write your comment...'}),
+            'tags': TagWidget(),
         }
