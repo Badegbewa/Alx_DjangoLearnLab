cb3f2ad32f5df94ee47405c5bdacf485f1cfe80a
Adegbewa Beyioku <beyiokuenny@gmail.com>
2025-08-24 18:46:39 +0100

    change the database from SQL to Postgresql database on settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -74,7 +74,18 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+DATABASES =DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_blog_db',      
+        'USER': 'blog_user',          
+        'PASSWORD': '1759', 
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+ 
+{
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
