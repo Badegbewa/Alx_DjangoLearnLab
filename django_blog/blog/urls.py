0aa10da97f094dc98828b5998aca97465024c9bd
Adegbewa Beyioku <beyiokuenny@gmail.com>
2025-08-24 21:25:24 +0100

    made minor changes to url path for commentview in blog app urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import home, register_view, profile_view
-from .views import add_comment, CommentDeleteView, CommentUpdateView
+from .views import CommentCreateView, CommentDeleteView, CommentUpdateView
 from .views import (
     PostListView, 
     PostDetailView, 
@@ -25,7 +25,7 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
-    path('post/<int:pk>/comment/new/', add_comment, name='add-comment'),
+    path('post/<int:pk>/comments/new/',CommentCreateView.as_view(), name='add-comment'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
